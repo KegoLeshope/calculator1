9f01bb4ca2bb8660f688252345c232684b4966ae
KegoLeshope <leshopek48@gmail.com>
2022-02-06 20:05:57 +0200

    First commit

---
+++ src/za/ac/cput/calculator1/Division.java
@@ -0,0 +1,12 @@
+package za.ac.cput.calculator1;
+
+
+/*
+Returns result of integer division
+ */
+public class Division {
+    public static int integerDivision( int numerator, int denominator){
+        if (denominator <1) return 0;
+        return numerator/denominator;
+    }
+}
